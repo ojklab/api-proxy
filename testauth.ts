fe8f617a5dc3dbe96f6e47fa65e5fed10d39df97
OJK <26958104+ojklab@users.noreply.github.com>
2020-11-23 17:15:00 +0900

    init

---
+++ testauth.ts
@@ -0,0 +1,24 @@
+import { basicAuth, createApp } from "https://servestjs.org/@v1.1.7/mod.ts";
+const app = createApp();
+// Add global auth middleware
+app.use(
+  basicAuth({
+    credentials: [{
+      username: "deno",
+      password: "deno is nice",
+    }, {
+      username: "node",
+      password: "node is awesome",
+    }],
+  }),
+);
+app.get("/", async (req) => {
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/plain",
+    }),
+    body: "Hello, Servest!",
+  });
+});
+app.listen({ port: 8899 });
